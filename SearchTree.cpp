0ff4b910380d66f7a02bfa0d6ec190110e8f8678
Muh Arya Saputra <aryaom74@gmail.com>
2026-06-11 18:52:48 +0700

    Update SearchTree.cpp

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -95,4 +95,19 @@ class BinaryTree
             inorder(ptr->righchild);
         }
     }
+    void preorder (Node *ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "tree is empty" << endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            cout << ptr->info << " ";
+            preorder(ptr->leftchild);
+            preorder(ptr->righchild);
+        }
+    }
     
